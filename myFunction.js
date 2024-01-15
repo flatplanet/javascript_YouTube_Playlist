e67ea24e792b5c931fd7c0ba1d6dfdb02a28fa28
John Elder <johne4196@gmail.com>
2024-01-15 08:18:38 -0800

    Functions;

---
+++ myFunction.js
@@ -0,0 +1,5 @@
+function hello(firstName, lastName){
+            
+            return ("Hello " + firstName + " " + lastName + "!"  + "<br/><br/>");
+            //document.write("Hello World"  + "<br/><br/>");
+        }
